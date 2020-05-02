aa4b391ecc55e3fd9264a2900374fa84468d1c8b
Paranthaman <52395019+Params456@users.noreply.github.com>
2020-05-03 00:32:12 +0530

    Update index.js

--- Inventry/index.js
+++ Inventry/index.js
@@ -21,7 +21,7 @@ app.use((req,res,next)=>{
 })
 
 app.use((error,req,res,next)=>{
-    error.status = (error.status || 500 );
+    res.status(error.status || 500 );
     res.json({
         message:error.message
     })
